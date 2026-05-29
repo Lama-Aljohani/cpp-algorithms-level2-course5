a4b636a89e0988eb70ae0a6fadaf09fb5d4b1013
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2026-05-29 20:25:28 +0300

    Update assignment 16/2

--- assignmentslevel2/assignment16-2/assignment16-2/assignment16-2.cpp
+++ assignmentslevel2/assignment16-2/assignment16-2/assignment16-2.cpp
@@ -1,20 +1,34 @@
-// assignment16-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
+
+void PrintAllFromAAAtoZZZ() {
+
+	cout << "\n";
+	string word = " ";
+
+	for (int i = 65; i <= 90; i++) {
+		
+		for (int j = 65; j <= 90; j++) {
+			for (int k = 65; k <= 90; k++) {
+
+
+				word.append(1, char(i));
+				word.append(1, char(j));
+				word.append(1, char(k));
+
+				cout << word << endl;
+				word = "";
+			}
+		}
+		
+		cout << "\n___________________________________\n";
+	}
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+	PrintAllFromAAAtoZZZ();
+
+	return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
